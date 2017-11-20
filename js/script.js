7cf591f67a13d091a586d33aa42032c018b839b0
Smaleva <smaleva.evgenia@yandex.com>
2017-11-20 14:06:12 +0300

    last shit

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ var closeModal = document.querySelector('.close-window');
 link.addEventListener('click', function(event) {
     event.preventDefault();
     modal.classList.toggle('open');
+    HTMLBodyElement.classList.toggle('shadow');
 });
 
 closeModal.addEventListener('click', function() {
